fba00ff38f7a9208213bd4c26fe9cd50e767a59c
Guðjón Guðjónsson <gudjon@kolibri.is>
2025-05-05 22:05:41 +0000

    Rewrites tas-authority api

--- src/app/api/internal/tax-authority/route.ts
+++ src/app/api/internal/tax-authority/route.ts
@@ -1,35 +1,82 @@
+import { route, routeOperation, TypedNextResponse } from 'next-rest-framework'
+import { z } from 'zod'
 import postgres from 'postgres'
+
 const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' })
 
-async function getTaxPayer(nationalId: string) {
-  const data = await sql`
-    SELECT *
-    FROM tax_authority_tax_payers
-    WHERE national_id = ${nationalId};
-  `
+const taxpayerSchema = z.object({
+  national_id: z.string(),
+  email: z.string(),
+})
 
-  return data
-}
+export const { GET } = route({
+  getTaxPayer: routeOperation({
+    method: 'GET',
+  })
+    .input({
+      query: z.object({
+        nationalId: z.string(),
+      }),
+    })
+    .outputs([
+      {
+        status: 200,
+        contentType: 'application/json',
+        body: z.array(taxpayerSchema),
+      },
+      {
+        status: 400,
+        contentType: 'application/json',
+        body: z.object({ error: z.string() }),
+      },
+      {
+        status: 401,
+        contentType: 'application/json',
+        body: z.object({ error: z.string() }),
+      },
+      {
+        status: 500,
+        contentType: 'application/json',
+        body: z.object({ error: z.string() }),
+      },
+    ])
+    .middleware(async (req) => {
+      const secret = req.headers.get('x-internal-secret')
+      if (secret !== process.env.INTERNAL_API_SECRET) {
+        return TypedNextResponse.json(
+          { error: 'Unauthorized' },
+          { status: 401 },
+        )
+      }
+    })
+    .handler(async (req, ctx, params) => {
+      const url = new URL(req.url)
+      const nationalId = url.searchParams.get('nationalId')
+      console.log('Received request:', nationalId)
+      // const { nationalId } = { nationalId: undefined }
 
-export async function GET(request: Request) {
-  const secret = request.headers.get('x-internal-secret')
-  if (secret !== process.env.INTERNAL_API_SECRET) {
-    return new Response('Unauthorized', { status: 401 })
-  }
+      if (!nationalId) {
+        return TypedNextResponse.json(
+          { error: 'Missing nationalId' },
+          { status: 400 },
+        )
+      }
 
-  const { searchParams } = new URL(request.url)
+      try {
+        type TaxPayer = z.infer<typeof taxpayerSchema>
 
-  const nationalId = searchParams.get('nationalId')
-
-  if (!nationalId) {
-    return Response.json({ error: 'Missing nationalId' }, { status: 400 })
-  }
-
-  try {
-    const result = await getTaxPayer(nationalId)
-
-    return Response.json(result)
-  } catch (error) {
-    return Response.json({ error }, { status: 500 })
-  }
-}
+        const data = await sql<TaxPayer[]>`
+          SELECT national_id, email
+          FROM tax_authority_tax_payers
+          WHERE national_id = ${nationalId};
+        `
+        return TypedNextResponse.json(data, { status: 200 })
+      } catch (error) {
+        console.error('Error fetching data:', error)
+        return TypedNextResponse.json(
+          { error: 'Internal Server Error' },
+          { status: 500 },
+        )
+      }
+    }),
+})
