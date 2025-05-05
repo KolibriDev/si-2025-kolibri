7f2b0f46a512d3f70b348e61e9510d8d3378150e
Guðjón Guðjónsson <gudjon@kolibri.is>
2025-05-05 20:23:03 +0000

    GraphQL codegen

---
+++ codegen.ts
@@ -0,0 +1,26 @@
+import type { CodegenConfig } from '@graphql-codegen/cli'
+
+const config: CodegenConfig = {
+  overwrite: true,
+  schema: 'http://localhost:3000/api/graphql',
+  documents: ['src/**/*.graphql', 'src/**/*.tsx'],
+  generates: {
+    'src/generated/graphql.ts': {
+      plugins: [
+        'typescript',
+        'typescript-operations',
+        'typescript-react-apollo',
+      ],
+      config: {
+        withHooks: true,
+        withHOC: false,
+        withComponent: false,
+      },
+    },
+    './graphql.schema.json': {
+      plugins: ['introspection'],
+    },
+  },
+}
+
+export default config
