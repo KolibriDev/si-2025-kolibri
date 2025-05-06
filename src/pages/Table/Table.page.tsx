d86dc76da5fe7111638c978cddcea5cf2385f7e5
Arnar Kári <arnarkari93@gmail.com>
2025-05-06 16:06:20 +0000

    feat: table expand

--- src/pages/Table/Table.page.tsx
+++ src/pages/Table/Table.page.tsx
@@ -2,7 +2,10 @@
 
 import * as T from '@/components/Table/Table'
 import { Box } from '@/components/Box/Box'
+import * as G from '@/components/Grid'
 import { Button } from '@/components/Button/Button'
+import { Text } from '@/components/Text/Text'
+import AnimateHeight from 'react-animate-height'
 
 import { formatISK } from '@/lib/utils'
 import { useState } from 'react'
@@ -116,13 +119,85 @@ function TableAdd() {
   )
 }
 
+type morage = {
+  lender: string
+  number: string
+  interestExpenses: number
+  remainingDebt: number
+}
+function TableExpand({ subtable }: { subtable: React.ReactNode }) {
+  const morage: morage = {
+    lender: 'Íslandsbanki hf.',
+    number: '56783900123',
+    interestExpenses: 920_000,
+    remainingDebt: 28_540_000,
+  }
+
+  const [expanded, toogleExpanded] = useState<boolean>(false)
+
+  return (
+    <Box>
+      <T.Table>
+        <T.Head>
+          <T.Row>
+            <T.HeadData>{/* empty for expand button */}</T.HeadData>
+            <T.HeadData>{'Lánveitandi'}</T.HeadData>
+            <T.HeadData>{'Lánsnúmer'}</T.HeadData>
+            <T.HeadData align="right">{'Vaxtagjöld'}</T.HeadData>
+            <T.HeadData align="right">{'Eftirstöðvar skulda'}</T.HeadData>
+          </T.Row>
+        </T.Head>
+        <T.Body>
+          <T.Row>
+            <T.Data>
+              <Button
+                circle
+                title="Expand"
+                type="light"
+                icon={expanded ? 'remove' : 'add'}
+                onClick={() => toogleExpanded((prev) => !prev)}
+              />
+            </T.Data>
+            <T.Data>{morage.lender}</T.Data>
+            <T.Data>{morage.number}</T.Data>
+            <T.Data align="right">{formatISK(morage.interestExpenses)}</T.Data>
+            <T.Data align="right">{formatISK(morage.remainingDebt)}</T.Data>
+          </T.Row>
+          <T.Row>
+            <T.Data
+              colSpan={5}
+              borderColor="transparent"
+              style={{ padding: 0 }}
+            >
+              <AnimateHeight duration={300} height={expanded ? 'auto' : 0}>
+                {subtable}
+              </AnimateHeight>
+            </T.Data>
+          </T.Row>
+        </T.Body>
+        <T.Foot>
+          <T.Row>
+            <T.Data text={{ fontWeight: 'bold' }}>Samtals</T.Data>
+            <T.Data>{/* empty for lanveitandi */}</T.Data>
+            <T.Data>{/* empty  for lansnumber */}</T.Data>
+            <T.Data text={{ fontWeight: 'bold' }} align="right">
+              {formatISK(morage.interestExpenses)}
+            </T.Data>
+            <T.Data text={{ fontWeight: 'bold' }} align="right">
+              {formatISK(morage.remainingDebt)}
+            </T.Data>
+          </T.Row>
+        </T.Foot>
+      </T.Table>
+    </Box>
+  )
+}
+
 export default function TablePage() {
   return (
     <Box style={{ maxWidth: '1000px', margin: 'auto', marginTop: '24px' }}>
-      <h2>Table basic</h2>
-      <TableBasic />
-      <h2>Table add</h2>
-      <TableAdd />
+      <h2>Table expand</h2>
+      <TableExpand subtable={<Text>{'Todo'}</Text>} />
     </Box>
   )
 }
