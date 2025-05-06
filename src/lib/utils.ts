4890d7bf4479e63fbf7c3a69a97fbe73bd022001
Arnar Kári <arnarkari93@gmail.com>
2025-05-06 22:23:57 +0000

    feat: add formatDateIS

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,3 +1,11 @@
 export function formatISK(amount: number): string {
   return amount.toLocaleString('is-IS', { maximumFractionDigits: 0 }) + ' kr.'
 }
+
+export function formatDateIS(date: Date): string {
+  return date.toLocaleDateString('is-IS', {
+    day: '2-digit',
+    month: '2-digit',
+    year: 'numeric',
+  })
+}
