35be07093e68c4a7d15cd74bafe260df81b267c0
Telma Guðbjörg Eyþórsdóttir <telma@kolibri.is>
2025-05-06 14:24:08 +0000

    feat: add steps

---
+++ src/components/steps/Fasteignir.tsx
@@ -0,0 +1,14 @@
+'use client'
+
+import React from 'react'
+import { Text } from '@/components/Text/Text'
+
+const Fasteignir = () => {
+  return (
+    <div>
+      <Text>TODO</Text>
+    </div>
+  )
+}
+
+export default Fasteignir
---
+++ src/components/steps/Upplysingar.tsx
@@ -0,0 +1,21 @@
+'use client'
+
+import React from 'react'
+import { Text } from '@/components/Text/Text'
+
+const Upplysingar = () => {
+  return (
+    <div>
+      <Text>
+        Þú ert að hefja skil á einfaldri útgáfu skattframtals. Á Ísland.is er
+        eingöngu hægt að yfirfara fyrirfram skráðar upplýsingar byggðar á gögnum
+        sem hafa borist Skattinum um tekjur þínar, eignir og skuldir. Þú getur
+        bætt við tekjum og frádráttarliðum en til að leiðrétta aðra liði eða
+        skila fylgiskjölum þarftu að opna almennt skattframtal á þjónustuvef
+        Skattsins.
+      </Text>
+    </div>
+  )
+}
+
+export default Upplysingar
