fcf729a2a61c92e7341e6e0a094dc8f93daa8f21
Telma Guðbjörg Eyþórsdóttir <telma@kolibri.is>
2025-05-06 14:28:07 +0000

    feat: create layout and page for steps in framtal

---
+++ src/app/framtal/[id]/[step]/layout.css.ts
@@ -0,0 +1,18 @@
+import { style } from '@vanilla-extract/css'
+
+export const stepLayout = style({
+  display: 'flex',
+  flexDirection: 'column',
+  gap: '1px',
+})
+
+export const container = style({
+  display: 'flex',
+  flexDirection: 'column',
+  gap: '16px',
+  minHeight: '776px',
+})
+
+export const content = style({
+  flex: 1,
+})
