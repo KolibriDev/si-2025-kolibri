dd19e8032ec80a5bdcc3ffcb90decd434d08da14
Ívar Oddsson <ivaroddsson@gmail.com>
2025-05-06 17:15:56 +0000

    Start e2e testing flow

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,10 +1,15 @@
 import { test, expect } from '@playwright/test'
 
 test('get started link', async ({ page }) => {
-  await page.goto('https://si-2025-kolibri.vercel.app/')
+  await page.goto(
+    'https://si-2025-kolibri.vercel.app/framtal/feikID/upplysingar',
+  )
 
   // Expects page to have a heading with the name of Installation.
   await expect(
-    page.getByRole('heading', { level: 1, name: 'TestC' }),
+    page.getByRole('heading', {
+      level: 1,
+      name: 'Þú ert að fara að skila skattframtali',
+    }),
   ).toBeVisible()
 })
