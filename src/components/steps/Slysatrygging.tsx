3902867cdbe7ece8bc12ab9dd0c5bc561bfbd37e
Arnar Kári <arnarkari93@gmail.com>
2025-05-06 19:15:45 +0000

    feat: add page slysatrygging

--- src/components/steps/Slysatrygging.tsx
+++ src/components/steps/Slysatrygging.tsx
@@ -1,12 +1,48 @@
 'use client'
 
-import React from 'react'
+import React, { useState } from 'react'
 import { Text } from '@/components/Text/Text'
+import { RadioButton } from '../RadioButton/RadioButton'
+
+import * as styles from './Slysatrygging.css'
 
 const Slysatrygging = () => {
+  const [state, setState] = useState<'yes' | 'no' | undefined>(undefined)
   return (
     <div>
-      <Text>TODO</Text>
+      <Text marginBottom={2}>
+        Þú getur valið að óska eftir slysatryggingu vegna heimilisstarfa.
+        Tryggingin veitir þér sömu vernd frá almannatryggingum og trygging fyrir
+        vinnuslysi ef slys ber að höndum við ólaunuð heimilisstörf, svo sem
+        þrif, viðhald eða garðvinnu á eigin heimili.{' '}
+      </Text>
+      <Text marginBottom={6}>
+        Iðgjald fyrir trygginguna er 550 kr. fyrir hvern einstakling á ári.
+      </Text>
+
+      <div>
+        <Text variant="h4" marginBottom={2}>
+          Má bjóða þér slysatryggingu vegna heimilisstarfa?
+        </Text>
+        <div className={styles.inputContainer}>
+          <RadioButton
+            name="yes"
+            backgroundColor="blue"
+            label="Já, ég vil fá slysatryggingu"
+            large
+            onChange={() => setState('yes')}
+            checked={state === 'yes'}
+          />
+          <RadioButton
+            name="no"
+            backgroundColor="blue"
+            label="Nei, ég afþakka slysatryggingu"
+            large
+            onChange={() => setState('no')}
+            checked={state === 'no'}
+          />
+        </div>
+      </div>
     </div>
   )
 }
