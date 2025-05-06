2190dd5caf16496a78f51c1c0ca5feaa1300d74a
Ívar Oddsson <ivaroddsson@gmail.com>
2025-05-06 20:37:14 +0000

    Add context

--- src/components/steps/Launagreidslur.tsx
+++ src/components/steps/Launagreidslur.tsx
@@ -6,6 +6,7 @@ import { Box } from '@/components/Box/Box'
 import * as T from '@/components/Table/Table'
 import { Button } from '@/components/Button/Button'
 import { formatISK } from '@/lib/utils'
+import { useTaxContext } from '../Utils/context/taxContext'
 
 type vehicle = {
   licenceNumber: string
@@ -14,25 +15,11 @@ type vehicle = {
 }
 
 const Launagreidslur = () => {
-  const [vehicles, setVehicles] = useState<vehicle[]>([
-    {
-      licenceNumber: 'KB-521',
-      yearPurchesed: '2021',
-      price: 3_100_000,
-    },
-    {
-      licenceNumber: 'JU-329',
-      yearPurchesed: '2012',
-      price: 430_000,
-    },
-  ])
+  const { taxReturn } = useTaxContext()
 
-  const onClick = () => {
-    setVehicles((prev) => [
-      ...prev,
-      { licenceNumber: 'VY-V79', yearPurchesed: '2018', price: 1_200_000 },
-    ])
-  }
+  console.log(taxReturn)
+
+  const salaryEntries = taxReturn?.salaries
 
   return (
     <div>
@@ -45,51 +32,54 @@ const Launagreidslur = () => {
         Ef þú fékkst fleiri launa- eða verktakagreiðslur á árinu þarftu að skrá
         þær til að framtalið sé rétt.
       </Text>
-      <Box marginBottom={3}>
-        <T.Table>
-          <T.Head>
-            <T.Row>
-              <T.HeadData>{/* empty */}</T.HeadData>
-              <T.HeadData>{'Launagreiðandi'}</T.HeadData>
-              <T.HeadData>{'Kennitala'}</T.HeadData>
-              <T.HeadData align="right">{'Launafjárhæð'}</T.HeadData>
-            </T.Row>
-          </T.Head>
-          <T.Body>
-            {vehicles.map((v) => (
-              <T.Row key={v.licenceNumber}>
-                <T.Data>
-                  <Button
-                    circle
-                    colorScheme="negative"
-                    title="Expand"
-                    type="icon"
-                    icon={'pencil'}
-                    onClick={() => {}}
-                  />
-                </T.Data>
-                <T.Data>{v.licenceNumber}</T.Data>
-                <T.Data>{v.yearPurchesed}</T.Data>
-                <T.Data align="right">{formatISK(v.price)}</T.Data>
-              </T.Row>
-            ))}
-          </T.Body>
-          <T.Foot>
-            <T.Row>
-              <T.Data text={{ fontWeight: 'bold' }}>Samtals:</T.Data>
-              <T.Data>{/* empty */}</T.Data>
-              <T.Data>{/* empty */}</T.Data>
-              <T.Data text={{ fontWeight: 'bold' }} align="right">
-                {formatISK(vehicles.reduce((v, a) => v + a.price, 0))}
-              </T.Data>
-            </T.Row>
-          </T.Foot>
-        </T.Table>
-      </Box>
-
-      <Button variant="ghost" size="small" onClick={onClick}>
-        Add +
-      </Button>
+      {salaryEntries && (
+        <>
+          <Box marginBottom={3}>
+            <T.Table>
+              <T.Head>
+                <T.Row>
+                  <T.HeadData>{/* empty */}</T.HeadData>
+                  <T.HeadData>{'Launagreiðandi'}</T.HeadData>
+                  <T.HeadData>{'Kennitala'}</T.HeadData>
+                  <T.HeadData align="right">{'Launafjárhæð'}</T.HeadData>
+                </T.Row>
+              </T.Head>
+              <T.Body>
+                {salaryEntries.map((salaryEntry) => (
+                  <T.Row key={salaryEntry.employerName}>
+                    <T.Data>
+                      <Button
+                        circle
+                        colorScheme="negative"
+                        title="Expand"
+                        type="icon"
+                        icon={'pencil'}
+                        onClick={() => {}}
+                      />
+                    </T.Data>
+                    <T.Data>{salaryEntry.amount}</T.Data>
+                    <T.Data>{salaryEntry.employerNationalId}</T.Data>
+                    <T.Data align="right">{'sdf'}</T.Data>
+                  </T.Row>
+                ))}
+              </T.Body>
+              <T.Foot>
+                <T.Row>
+                  <T.Data text={{ fontWeight: 'bold' }}>Samtals:</T.Data>
+                  <T.Data>{/* empty */}</T.Data>
+                  <T.Data>{/* empty */}</T.Data>
+                  <T.Data text={{ fontWeight: 'bold' }} align="right">
+                    {/* {formatISK(vehicles.reduce((v, a) => v + a.price, 0))} */}
+                  </T.Data>
+                </T.Row>
+              </T.Foot>
+            </T.Table>
+          </Box>
+          <Button variant="ghost" size="small" onClick={() => {}}>
+            Add +
+          </Button>
+        </>
+      )}
     </div>
   )
 }
