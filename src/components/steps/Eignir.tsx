35076e0ac012bb067cecd7bfd7d9c3d9e160f3e7
unakb <una@kolibri.is>
2025-05-07 16:52:49 +0000

    Update Eignir.tsx

--- src/components/steps/Eignir.tsx
+++ src/components/steps/Eignir.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import React, { useEffect } from 'react'
+import React from 'react'
 import { Text } from '@/components/Text/Text'
 import * as T from '@/components/Table/Table'
 import { useTaxContext } from '../Utils/context/taxContext'
