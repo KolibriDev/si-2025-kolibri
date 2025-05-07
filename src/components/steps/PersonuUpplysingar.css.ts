93f69192ad26771a6cbf400e830c4dd2523116b3
Ívar Oddsson <ivaroddsson@gmail.com>
2025-05-07 15:07:11 +0000

    Pages are mobile friendly

--- src/components/steps/PersonuUpplysingar.css.ts
+++ src/components/steps/PersonuUpplysingar.css.ts
@@ -3,13 +3,26 @@ import { theme } from '@/components/Theme/theme'
 
 export const tranparentCard = style({
   borderRadius: theme.border.radius.large,
-  padding: theme.spacing[4],
   border: '1px solid transparent',
   borderColor: theme.border.color.blue200,
+  padding: `${theme.spacing[3]}px ${theme.spacing[2]}px`,
+
+  '@media': {
+    [`screen and (min-width: ${theme.breakpoints.md}px)`]: {
+      padding: theme.spacing[4],
+    },
+  },
 })
 
 export const inputContainer = style({
   display: 'grid',
-  gridTemplateColumns: '1fr 1fr',
-  columnGap: theme.spacing[2],
+  gridTemplateColumns: '1fr',
+  rowGap: theme.spacing[2],
+
+  '@media': {
+    [`screen and (min-width: ${theme.breakpoints.md}px)`]: {
+      gridTemplateColumns: '1fr 1fr',
+      columnGap: theme.spacing[2],
+    },
+  },
 })
