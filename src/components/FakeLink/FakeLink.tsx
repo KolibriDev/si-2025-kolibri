ff0422cfd095393db26e18da15a54c55d57683d5
Ívar Oddsson <ivaroddsson@gmail.com>
2025-05-07 23:20:31 +0000

    Checkpoint

---
+++ src/components/FakeLink/FakeLink.tsx
@@ -0,0 +1,7 @@
+import { FC } from 'react'
+
+const FakeLink: FC = () => {
+  return <span>asdlk</span>
+}
+
+export default FakeLink
--- src/components/steps/Upplysingar.tsx
+++ src/components/steps/Upplysingar.tsx
@@ -7,28 +7,29 @@ import { Bullet, BulletList } from '../BulletList/BulletList'
 const Upplysingar = () => {
   return (
     <div>
-      <Text marginBottom={2}>
-        Þú ert að hefja skil á skattframtali þínu. Flestir framteljendur þurfa
-        eingöngu að yfirfara fyrirfram skráðar upplýsingar byggðar á gögnum sem
-        hafa borist Skattinum um tekjur, eignir og skuldir.
-      </Text>
+      <Text marginBottom={2}>Nokkrir punktar áður en þú byrjar:</Text>
       <BulletList>
+        <Bullet>
+          Flestir framteljendur þurfa eingöngu að yfirfara þau gögn sem hafa
+          verið send Skattinum um tekjur, eignir og skuldir.
+        </Bullet>
         <Bullet>
           Þú getur bætt við tekjum og frádráttarliðum, gert grein fyrir kaupum
           og sölu eigna og skráð upplýsingar um íbúðalán og greiðslur af þeim.
-          Til að leiðrétta aðra liði eða skila fylgiskjölum þarftu að opna
-          almennt skattframtal á þjónustuvef Skattsins.
         </Bullet>
         <Bullet>
-          Ef þú ert í hjónabandi, eða hefur óskað eftir samsköttun með maka,
-          færðu einnig upp tekjur fyrir maka. Eignir og skuldir eru
-          sameiginlegar á framtalinu.
+          Til að gera aðrar leiðréttingar eða fylla út rekstrareyðublöð þarftu
+          að opna almennt skattframtal á þjónustuvef Skattsins og skila því þar.
+          <span>asd</span>
         </Bullet>
         <Bullet>
           Samþykkt skattframtal á Ísland.is er sent í vinnslu til Skattsins.
           Hægt er að biðja um leiðréttingu á þjónustuvef Skattsins.
         </Bullet>
-        <Bullet>Hægt er að skoða fyrri framtöl á Mínum síðum.</Bullet>
+        <Bullet>
+          Hægt er að skoða framtöl á Mínum síðum á Ísland.is eftir að þau hafa
+          verið staðfest af Skattinum.
+        </Bullet>
       </BulletList>
     </div>
   )
