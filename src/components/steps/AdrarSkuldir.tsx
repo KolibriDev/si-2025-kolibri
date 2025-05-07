84ffe96a9151128733ef339d741b465dafe8fbd3
Telma Guðbjörg Eyþórsdóttir <telma@kolibri.is>
2025-05-07 13:35:01 +0000

    feat: first draft for samantekt

--- src/components/steps/AdrarSkuldir.tsx
+++ src/components/steps/AdrarSkuldir.tsx
@@ -7,6 +7,86 @@ import { Box } from '../Box/Box'
 import * as T from '@/components/Table/Table'
 import { Button } from '../Button/Button'
 import { formatISK, formatNationalId } from '@/lib/utils'
+import { OtherDebt } from '@/generated/graphql'
+import { Icon } from '../IconRC/Icon'
+
+export const OtherDebts = ({
+  otherDebts,
+  isEditable,
+}: {
+  otherDebts: OtherDebt[]
+  isEditable?: boolean
+}) => {
+  return (
+    <Box marginBottom={3}>
+      <T.Table>
+        <T.Head>
+          <T.Row>
+            {isEditable && <T.HeadData>{/* empty */}</T.HeadData>}
+            <T.HeadData>{'Nafn lánveitanda'}</T.HeadData>
+            <T.HeadData>{'Kennitala lánveitanda'}</T.HeadData>
+            <T.HeadData>{'Vaxtagjöld'}</T.HeadData>
+            <T.HeadData align="right">{'Eftirstöðvar skulda'}</T.HeadData>
+          </T.Row>
+        </T.Head>
+        <T.Body>
+          {otherDebts.map((otherDept) => (
+            <T.Row key={otherDept.lenderName}>
+              {isEditable && (
+                <T.Data>
+                  <Button
+                    circle
+                    colorScheme="negative"
+                    title="Breyta"
+                    type="icon"
+                    icon={'pencil'}
+                    onClick={() => {}}
+                  />
+                </T.Data>
+              )}
+              <T.Data>{otherDept.lenderName}</T.Data>
+              <T.Data>{formatNationalId(otherDept.lenderNationalId)}</T.Data>
+              <T.Data align="right">
+                {formatISK(otherDept.interestPayments)}
+              </T.Data>
+              <T.Data align="right">
+                {formatISK(otherDept.remainingBalance)}
+              </T.Data>
+            </T.Row>
+          ))}
+        </T.Body>
+        <T.Foot>
+          <T.Row>
+            <T.Data text={{ fontWeight: 'bold' }} noBorderBottom>
+              Samtals:
+            </T.Data>
+            {isEditable && <T.Data noBorderBottom>{/* empty */}</T.Data>}
+            <T.Data noBorderBottom>{/* empty */}</T.Data>
+            <T.Data
+              text={{ fontWeight: 'bold', whiteSpace: 'nowrap' }}
+              noBorderBottom
+            >
+              {formatISK(
+                // TODO: FIX ! below
+                otherDebts.reduce((v, a) => v + a.interestPayments! || 0, 0),
+              )}
+            </T.Data>
+            <T.Data
+              text={{ fontWeight: 'bold', whiteSpace: 'nowrap' }}
+              align="right"
+              noBorderBottom
+            >
+              {formatISK(
+                // TODO: FIX ! below
+                otherDebts.reduce((v, a) => v + a.remainingBalance! || 0, 0),
+              )}
+            </T.Data>
+          </T.Row>
+        </T.Foot>
+      </T.Table>
+    </Box>
+  )
+}
 
 const AdrarSkuldir = () => {
   const { taxReturn } = useTaxContext()
@@ -22,80 +102,15 @@ const AdrarSkuldir = () => {
         Ef þú skuldar öðrum lánveitendum en það sem fram kemur hér þarftu að
         bæta skuldinni við til að framtalið sé rétt.
       </Text>
-      {otherDepts && (
-        <>
-          <Box marginBottom={3}>
-            <T.Table>
-              <T.Head>
-                <T.Row>
-                  <T.HeadData>{/* empty */}</T.HeadData>
-                  <T.HeadData>{'Nafn lánveitanda'}</T.HeadData>
-                  <T.HeadData>{'Kennitala lánveitanda'}</T.HeadData>
-                  <T.HeadData>{'Vaxtagjöld'}</T.HeadData>
-                  <T.HeadData align="right">{'Eftirstöðvar skulda'}</T.HeadData>
-                </T.Row>
-              </T.Head>
-              <T.Body>
-                {otherDepts.map((otherDept) => (
-                  <T.Row key={otherDept.lenderName}>
-                    <T.Data>
-                      <Button
-                        circle
-                        colorScheme="negative"
-                        title="Breyta"
-                        type="icon"
-                        icon={'pencil'}
-                        onClick={() => {}}
-                      />
-                    </T.Data>
-                    <T.Data>{otherDept.lenderName}</T.Data>
-                    <T.Data>
-                      {formatNationalId(otherDept.lenderNationalId)}
-                    </T.Data>
-                    <T.Data align="right">
-                      {formatISK(otherDept.interestPayments)}
-                    </T.Data>
-                    <T.Data align="right">
-                      {formatISK(otherDept.remainingBalance)}
-                    </T.Data>
-                  </T.Row>
-                ))}
-              </T.Body>
-              <T.Foot>
-                <T.Row>
-                  <T.Data text={{ fontWeight: 'bold' }}>Samtals:</T.Data>
-                  <T.Data>{/* empty */}</T.Data>
-                  <T.Data>{/* empty */}</T.Data>
-                  <T.Data text={{ fontWeight: 'bold', whiteSpace: 'nowrap' }}>
-                    {formatISK(
-                      // TODO: FIX ! below
-                      otherDepts.reduce(
-                        (v, a) => v + a.interestPayments! || 0,
-                        0,
-                      ),
-                    )}
-                  </T.Data>
-                  <T.Data
-                    text={{ fontWeight: 'bold', whiteSpace: 'nowrap' }}
-                    align="right"
-                  >
-                    {formatISK(
-                      // TODO: FIX ! below
-                      otherDepts.reduce(
-                        (v, a) => v + a.remainingBalance! || 0,
-                        0,
-                      ),
-                    )}
-                  </T.Data>
-                </T.Row>
-              </T.Foot>
-            </T.Table>
-          </Box>
-          <Button variant="ghost" size="small" icon="add" onClick={() => {}}>
+      <OtherDebts otherDebts={otherDepts ?? []} isEditable />
+      <Button variant="ghost" size="small" onClick={() => {}}>
+        <Box display="flex" columnGap={1} alignItems="center">
+          <Text variant="h5" fontWeight="semiBold" color="blue400">
             Bæta við
-          </Button>
-        </>
-      )}
+          </Text>
+          <Icon icon="add" size="small" />
+        </Box>
+      </Button>
     </div>
   )
 }
--- src/components/steps/StyrkirOgHlunnindi.tsx
+++ src/components/steps/StyrkirOgHlunnindi.tsx
@@ -7,13 +7,16 @@ import { Box } from '../Box/Box'
 import * as T from '@/components/Table/Table'
 import { Button } from '../Button/Button'
 import { formatISK } from '@/lib/utils'
-import { BenefitType } from '@/generated/graphql'
+import { BenefitInput, BenefitType } from '@/generated/graphql'
+import { Icon } from '../IconRC/Icon'
 
-const StyrkirOgHlunnindi = () => {
-  const { taxReturn } = useTaxContext()
-
-  const benefits = taxReturn?.benefits
-
+export const Benefits = ({
+  benefits,
+  isEditable = false,
+}: {
+  benefits: BenefitInput[]
+  isEditable?: boolean
+}) => {
   const mapBenefitType = (benefitType?: BenefitType | null) => {
     switch (benefitType) {
       case BenefitType.DailyAllowance:
@@ -28,6 +31,68 @@ const StyrkirOgHlunnindi = () => {
         return 'Ekki skráð'
     }
   }
+  return (
+    <>
+      <Box marginBottom={3}>
+        <T.Table>
+          <T.Head>
+            <T.Row>
+              {isEditable && <T.HeadData>{/* empty */}</T.HeadData>}
+              <T.HeadData>{'Nafn greiðanda'}</T.HeadData>
+              <T.HeadData>{'Hlunnindi eða styrkur'}</T.HeadData>
+              <T.HeadData align="right">{'Upphæð'}</T.HeadData>
+            </T.Row>
+          </T.Head>
+          <T.Body>
+            {benefits.map((benefit, index) => (
+              <T.Row key={`${benefit.payerName}-${index}`}>
+                {isEditable && (
+                  <T.Data>
+                    <Button
+                      circle
+                      colorScheme="negative"
+                      title="Breyta"
+                      type="icon"
+                      icon={'pencil'}
+                      onClick={() => {}}
+                    />
+                  </T.Data>
+                )}
+                <T.Data>{benefit.payerName}</T.Data>
+                <T.Data>{mapBenefitType(benefit.benefitType)}</T.Data>
+                <T.Data align="right">{formatISK(benefit.amount)}</T.Data>
+              </T.Row>
+            ))}
+          </T.Body>
+          <T.Foot>
+            <T.Row>
+              <T.Data text={{ fontWeight: 'bold' }} noBorderBottom>
+                Samtals:
+              </T.Data>
+              {isEditable && <T.Data noBorderBottom>{/* empty */}</T.Data>}
+              <T.Data noBorderBottom>{/* empty */}</T.Data>
+              <T.Data
+                text={{ fontWeight: 'bold' }}
+                align="right"
+                noBorderBottom
+              >
+                {
+                  //TODO: Fix ! below
+                  formatISK(benefits.reduce((v, a) => v + a.amount!, 0))
+                }
+              </T.Data>
+            </T.Row>
+          </T.Foot>
+        </T.Table>
+      </Box>
+    </>
+  )
+}
+
+const StyrkirOgHlunnindi = () => {
+  const { taxReturn } = useTaxContext()
+
+  const benefits = taxReturn?.benefits
 
   return (
     <div>
@@ -40,57 +105,15 @@ const StyrkirOgHlunnindi = () => {
         Ef þú fékkst greiddan styrk sem ekki kemur fram hér fyrir neðan þarftu
         að skrá hann til að framtalið sé rétt.
       </Text>
-      {benefits && (
-        <>
-          <Box marginBottom={3}>
-            <T.Table>
-              <T.Head>
-                <T.Row>
-                  <T.HeadData>{/* empty */}</T.HeadData>
-                  <T.HeadData>{'Nafn greiðanda'}</T.HeadData>
-                  <T.HeadData>{'Hlunnindi eða styrkur'}</T.HeadData>
-                  <T.HeadData align="right">{'Upphæð'}</T.HeadData>
-                </T.Row>
-              </T.Head>
-              <T.Body>
-                {benefits.map((benefit, index) => (
-                  <T.Row key={`${benefit.payerName}-${index}`}>
-                    <T.Data>
-                      <Button
-                        circle
-                        colorScheme="negative"
-                        title="Breyta"
-                        type="icon"
-                        icon={'pencil'}
-                        onClick={() => {}}
-                      />
-                    </T.Data>
-                    <T.Data>{benefit.payerName}</T.Data>
-                    <T.Data>{mapBenefitType(benefit.benefitType)}</T.Data>
-                    <T.Data align="right">{formatISK(benefit.amount)}</T.Data>
-                  </T.Row>
-                ))}
-              </T.Body>
-              <T.Foot>
-                <T.Row>
-                  <T.Data text={{ fontWeight: 'bold' }}>Samtals:</T.Data>
-                  <T.Data>{/* empty */}</T.Data>
-                  <T.Data>{/* empty */}</T.Data>
-                  <T.Data text={{ fontWeight: 'bold' }} align="right">
-                    {
-                      //TODO: Fix ! below
-                      formatISK(benefits.reduce((v, a) => v + a.amount!, 0))
-                    }
-                  </T.Data>
-                </T.Row>
-              </T.Foot>
-            </T.Table>
-          </Box>
-          <Button variant="ghost" size="small" onClick={() => {}}>
-            Add +
-          </Button>
-        </>
-      )}
+      <Benefits benefits={benefits ?? []} isEditable={true} />
+      <Button variant="ghost" size="small" onClick={() => {}}>
+        <Box display="flex" columnGap={1} alignItems="center">
+          <Text variant="h5" fontWeight="semiBold" color="blue400">
+            Bæta við
+          </Text>
+          <Icon icon="add" size="small" />
+        </Box>
+      </Button>
     </div>
   )
 }
